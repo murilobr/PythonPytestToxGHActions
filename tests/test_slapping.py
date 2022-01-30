769afbcd4ca90cdb154815d2eb16a2e70126bf2e
Murilo da Silva <murilobr@gmail.com>
2022-01-30 16:00:07 -0300

    test: add more tests and separate them by context

--- tests/test_slapping.py
+++ tests/test_slapping.py
@@ -1,14 +1,52 @@
+import pytest
 from slapping.slap_that_like_button import LikeState, slap_many
 
 
-def test_slaps():
+def test_empty_slap():
     assert slap_many(LikeState.empty, "") is LikeState.empty
+
+
+def test_single_slaps():
     assert slap_many(LikeState.empty, "l") is LikeState.liked
     assert slap_many(LikeState.empty, "d") is LikeState.disliked
-    assert slap_many(LikeState.empty, "ll") is LikeState.empty
-    assert slap_many(LikeState.empty, "dd") is LikeState.empty
-    assert slap_many(LikeState.empty, "ld") is LikeState.disliked
-    assert slap_many(LikeState.empty, "dl") is LikeState.liked
-    assert slap_many(LikeState.empty, "ldd") is LikeState.empty
-    assert slap_many(LikeState.empty, "lldd") is LikeState.empty
-    assert slap_many(LikeState.empty, "ddl") is LikeState.liked
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("ll", LikeState.empty),
+        ("dd", LikeState.empty),
+        ("ld", LikeState.disliked),
+        ("dl", LikeState.liked),
+        ("ldd", LikeState.empty),
+        ("lldd", LikeState.empty),
+        ("ddl", LikeState.liked),
+    ],
+)
+def test_multi_slaps(test_input, expected):
+    assert slap_many(LikeState.empty, test_input) is expected
+
+
+@pytest.mark.skip(reason="regexes not supported yet")
+def test_regex_slaps():
+    assert slap_many(LikeState.empty, "[ld]*ddl") is LikeState.liked
+
+
+@pytest.mark.xfail
+def test_divide_by_zero():
+    assert 1 / 0 == 1
+
+
+def test_invalid_slap():
+    with pytest.raises(ValueError):
+        slap_many(LikeState.empty, "x")
+
+
+# def test_many_slaps():
+#     assert slap_many(LikeState.empty, "ll") is LikeState.empty
+#     assert slap_many(LikeState.empty, "dd") is LikeState.empty
+#     assert slap_many(LikeState.empty, "ld") is LikeState.disliked
+#     assert slap_many(LikeState.empty, "dl") is LikeState.liked
+#     assert slap_many(LikeState.empty, "ldd") is LikeState.empty
+#     assert slap_many(LikeState.empty, "lldd") is LikeState.empty
+#     assert slap_many(LikeState.empty, "ddl") is LikeState.liked
