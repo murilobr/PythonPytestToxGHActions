c5eeb8a5b673c2b32cb2b77d054197c58d3d53dd
Murilo da Silva <murilobr@gmail.com>
2022-01-30 15:05:06 -0300

    chore: separate tests from code

---
+++ test_slapping.py
@@ -0,0 +1,14 @@
+from slap_that_like_button import LikeState, slap_many
+
+
+def test_slaps():
+    assert slap_many(LikeState.empty, "") is LikeState.empty
+    assert slap_many(LikeState.empty, "l") is LikeState.liked
+    assert slap_many(LikeState.empty, "d") is LikeState.disliked
+    assert slap_many(LikeState.empty, "ll") is LikeState.empty
+    assert slap_many(LikeState.empty, "dd") is LikeState.empty
+    assert slap_many(LikeState.empty, "ld") is LikeState.disliked
+    assert slap_many(LikeState.empty, "dl") is LikeState.liked
+    assert slap_many(LikeState.empty, "ldd") is LikeState.empty
+    assert slap_many(LikeState.empty, "lldd") is LikeState.empty
+    assert slap_many(LikeState.empty, "ddl") is LikeState.liked
